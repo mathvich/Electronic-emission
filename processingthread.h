30c0f7aaf63c5d4091cfdef0d0b7b280f580ad8a
mathvich <mathvich@gmail.com>
2022-06-10 13:48:55 +0300

    Code refactoring

--- processingthread.h
+++ processingthread.h
@@ -13,11 +13,11 @@ class processingThread : public QThread
 private:
     float dt;
     float T, U;
-    int Intensity;
-    float Frequency;
+    int intensity;
+    float frequency;
     unsigned long framesDone;
-    particle *a, *b, *arg, *k1, *k2, *k3, *k4;
-    particle *p;
+    particle *particles, *particlesAux, *k1, *k2, *k3, *k4;
+    particle *photons;
 
 public slots:
     void setdt(float _dt);
@@ -31,8 +31,8 @@ public:
     bool relaxation;
     void run();
     float Tcurr;
-    int Nparticles;
-    int Nphotons;
+    int particlesNumber;
+    int photonsNumber;
 
     processingThread(int _Nparticles, particle *array, int _Nphotons, particle *photons, float _dt=1e-4);
 
@@ -40,7 +40,7 @@ public:
     void photonEnabler(int _N, float _dt);
     void photonInteract();
     void setArray(particle *_a);
-    void fKernel(particle *in, particle *out, int i);
+    void forceKernel(particle *in, particle *out, int i);
     void sumKernel(particle *in1, particle *in2, particle *out, float dt, int i);
 };
 
